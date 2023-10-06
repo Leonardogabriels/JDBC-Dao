c1196f90f2b6d501130b6645c12e5e5faea0c3aa
leonardogabriels <leonardogabrielsilva1@outlook.com>
2023-10-06 08:10:22 -0300

    Class Department

--- src/application/program.java
+++ src/application/program.java
@@ -1,9 +1,17 @@
 package application;
 
+import java.sql.Connection;
+
+import db.DB;
+
 public class program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Connection conn = null;
+		
+		conn = DB.getConnection();
+		DB.closeConnection(); 
 
 	}
 
