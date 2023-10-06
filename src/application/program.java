b70a1cc8ef5f719e2a8cbb61c33ea846a984f85e
leonardogabriels <leonardogabrielsilva1@outlook.com>
2023-10-06 09:27:25 -0300

    class Seller

--- src/application/program.java
+++ src/application/program.java
@@ -1,18 +1,19 @@
 package application;
 
-import java.sql.Connection;
+import java.util.Date;
 
-import db.DB;
+
+import model.entities.Department;
+import model.entities.Seller;
 
 public class program {
-
+	
 	public static void main(String[] args) {
 		
-		Connection conn = null;
+		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "leo", "leo@gmail.com", new Date(),3000.0, obj);
 		
-		conn = DB.getConnection();
-		DB.closeConnection(); 
-
+		System.out.println(seller);
 	}
 
 }
